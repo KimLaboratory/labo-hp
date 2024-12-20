417613c52a692be85e3022cd3300dd14a61a571b
yamamoto99 <yamamoto1952hkb@gmail.com>
2024-12-20 23:26:40 +0900

    fix:Format correction and GithubActions correction

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import Navbar from './components/Navbar'
-import ImageSlider from './components/ImageSlider'
-import Introduction from './components/Introduction'
-import ResearchAreas from './components/ResearchAreas'
-import AboutLab from './components/AboutLab'
+import Navbar from "./components/Navbar";
+import ImageSlider from "./components/ImageSlider";
+import Introduction from "./components/Introduction";
+import ResearchAreas from "./components/ResearchAreas";
+import AboutLab from "./components/AboutLab";
 
 export default function App() {
   return (
@@ -17,5 +17,5 @@ export default function App() {
         </div>
       </div>
     </main>
-  )
+  );
 }
