dd2eb3ba543fab94d4487cb8267c2ff5c59f8b94
yamamoto99 <yamamoto1952hkb@gmail.com>
2024-12-22 12:38:52 +0900

    feat: Remove Gulp sitemap generation and integrate vite-plugin-sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
+import Sitemap from 'vite-plugin-sitemap'
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+	react(),
+	Sitemap({
+		hostname: 'https://www.kim-laboratory.net/',
+	})
+  ]
 });
